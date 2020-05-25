069e6ddbd5b577e2bcede0a4aada410919f3e47c
aliensmart <kaoua17@gmail.com>
2020-05-24 23:15:11 -0400

    added admin editor attriuts in author

--- admin/dashboard/admin.py
+++ admin/dashboard/admin.py
@@ -1,3 +1,32 @@
 from django.contrib import admin
+from .models import Author
 
 # Register your models here.
+@admin.register(Author)
+class AuthorAdmin(admin.ModelAdmin):
+    fields = ('name', 'title')
+
+    #use where the model fields is a date to make hierarchi
+    date_hierarchy = 'birth_date' 
+
+    #by default the actions are on the top
+    actions_on_top = True 
+
+    # if this is true the actions will be on the bottom
+    actions_on_bottom = False 
+
+    #Controls whether a selection counter is displayed next to the action dropdown
+    actions_selection_counter = True 
+
+    #This attribute overrides the default display value for record’s fields that are empty (None, empty string, etc.)
+    empty_value_display = '-empty-' 
+
+
+    #Set list_display to control which fields are displayed on the change list page of the admin.
+    list_display = ('name', 'title')
+
+# class AuthorAdmin(admin.ModelAdmin):
+#     exclude = ('birth_date', )
+
+class FlatPageAdmin(admin.ModelAdmin):
+    fields = ('url', 'title', 'content')
--- admin/dashboard/models.py
+++ admin/dashboard/models.py
@@ -3,5 +3,5 @@ from django.db import models
 # Create your models here.
 class Author(models.Model):
     name = models.CharField(max_length=100)
-    title = models.CharField(max_length=3)
+    title = models.CharField(max_length=10, null=True)
     birth_date = models.DateField(blank=True, null=True)
